1c6c342ed578f4b5ae9d8f9265e7d32fa69f2225
Dain-K <dksudi76@naver.com>
2022-10-05 23:55:53 +0900

    header, 이미지 생성

---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2022 International Workshop on Emerging ICT</title>
+    <link rel="stylesheet" href="./css/header.css">
+</head>
+<body>
+    <div class="header-box">
+        <h1 class="logo">
+            <a href="#">
+                <img src="./img/logo.png" alt="Yeungnam University Graduate School"/>
+            </a>
+        </h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
