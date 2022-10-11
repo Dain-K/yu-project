7157aa73ebff65842a3acfcd26ddf5dad84abf7c
Dain-K <dksudi76@naver.com>
2022-10-11 18:28:23 +0900

    information 추가

---
+++ detail.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>2022 International Workshop on Emerginig</title>
+    <link rel="stylesheet" href="./css/main.css" />
+    <link rel="stylesheet" href="./css/detail.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="box">
+        <p class="title">
+          Call for papers (2022 International Workshop on Emerging ICT)
+        </p>
+        <p>
+          The annual conference, Korea-Japan-China International Workshop will
+          be held in Yeungnam University, Korea in October 27-28, 2022. This
+          conference aims at improving the mutual understanding of educational
+          and research activities of the three universities (Yeungnam
+          University, Tohoku University, and Dalian University of Technology).
+          Since the first conference held at Yeungnam University in 2004, the
+          three universities have hosted this annual ceremony every year, taking
+          turns although since 2017, Dalian University of Technology has
+          replaced UESTC of China. Undergraduate and graduate students are
+          encouraged to participate in this academic meeting as well as the
+          faculty members. It will be a great opportunity for sharing their
+          university lives and recent research results in the area of
+          electronics and communications.
+        </p>
+        <p>
+          Topics of interest include all areas in Electrical and Electronics
+          Engineering, and Computer Science and Engineering, but are not limited
+          to:
+        </p>
+        <p>
+          Communication and coding<br />
+          Systems theory and control<br />
+          Optimization,<br />
+          Signal processing<br />
+          Device-level circuit design<br />
+          Energy, power, and electromagnetic systems<br />
+          Photonics and optics<br />
+          Superconductivity<br />
+          Solid state devices Electronic materials, microsystems and
+          nanotechnology<br />
+          Biomedical electronics<br />
+          Speech synthesis and recognition<br />
+          Electromechanical properties of biological cells<br />
+          Mathematical foundations of computation<br />
+          Cryptography and computer security<br />
+          Artificial intelligence<br />
+          Robotics, Vision, Machine learning<br />
+          Database systems<br />
+          Networks<br />
+          Computer architecture<br />
+          Operating systems
+        </p>
+        <p>
+          Instructions for manuscripts:<br />
+          Faculty members, graduate students, and undergraduate students of
+          Yeungnam University, Tohoku University and Dalian University of
+          Technology are cordially invited to submit their research results.
+          Manuscripts should be written in English and formatted according to
+          the template in the workshop website. The paper is limited at most as
+          5 pages. Manuscripts should be submitted via the conference website or
+          emailed to Technical program Co-chair (jchoi@yu.ac.kr).
+        </p>
+        <p>
+          Important dates are as follows.<br />
+          Manuscript due: October 2, 2022<br />
+          Notification of acceptance: October 17, 2022<br />
+          Conference date: October 27-28, 2022<br />
+          <br />
+          For other questions on paper submission, please contact us at:<br />
+          Dr. Jin-Ghoo Choi (Technical program Co-chair)<br />
+          E-mail: jchoi@yu.ac.kr<br />
+          Tel: +82-53-880-3517
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
