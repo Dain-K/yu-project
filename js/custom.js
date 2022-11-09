bf3f7182b5bb54eacdc59ada15d91f3b36f2a3f5
Dain-K <dksudi76@naver.com>
2022-11-09 15:30:57 +0900

    반응형 웹 제작

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function () {
-  console.log("hello");
-  $(".more-btn").click(function () {
-    $(this).toggleClass("active");
-    $(".main-navigation").toggleClass("active");
-  });
-});
+function showNav() {
+  console.log("h");
+  const nav = document.getElementById("nav").classList;
+  nav.toggle("active");
+  const btn = document.getElementById("btn").classList;
+  btn.toggle("active");
+}
